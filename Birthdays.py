d9f27bdad963e45d7b352b4e52a264303017fca1
Kevin <dsdevelopment@foxmail.com>
2018-07-24 09:34:42 +0800

    1. 字符串中字母出现的次数

--- Birthdays.py
+++ Birthdays.py
@@ -22,5 +22,18 @@ def getAgeWithName():
             birthdays[name] = age
             print('Birthday database updated!')
 
-getAgeWithName()
+# 获取字符串中字符出现的次数
+import pprint
+def countCharacter():
+    message = 'The setdefault() method is a nice shortcut to ensure that a key exists. Here is a short program that counts the number of occurrences of each letter in a string. Open the file editor window and enter the following code, saving it as characterCount.py:'
+    count = {}
+    for character in message:
+        count.setdefault(character,0)
+        count[character] += 1
+    # Pretty Print 完美输出
+    #pprint.pprint(count)
+    print(pprint.pformat(count))
+
+#getAgeWithName()
+countCharacter()
             
